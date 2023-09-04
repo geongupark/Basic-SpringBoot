9d9293867becb9a4b992559fc4824f0552f9dec7
geongupark <ggus.park@gmail.com>
2023-09-04 19:19:07 +0900

    Init springboot project using maven

---
+++ demobasic/src/main/java/com/gus/demobasic/DemobasicApplication.java
@@ -0,0 +1,13 @@
+package com.gus.demobasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobasicApplication.class, args);
+	}
+
+}
---
+++ demobasic/src/test/java/com/gus/demobasic/DemobasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gus.demobasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
